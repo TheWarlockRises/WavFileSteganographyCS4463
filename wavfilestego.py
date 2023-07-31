e551516403a75fef1fde81bbdb28cb3901a9017e
TheWarlockRises <treydamico3@gmail.com>
2023-07-31 18:23:54 -0500

    Added menu

--- wavfilestego.py
+++ wavfilestego.py
@@ -1,1 +1,23 @@
 import numpy as np
+
+def menu():
+    print("************WAV File Steganalysis**************")
+    print()
+
+    choice = input("""
+                        1. List Available Files for Steganalysis
+                        2. Analyze a File
+                   
+                        Please enter your choice: """)
+    
+    if choice == "1":
+        print("List")
+    elif choice == "2":
+        print("Analyze")
+    else:
+        print("Please only select 1 or 2")
+        print("Please try again")
+        menu()
+
+def main():
+    menu()
